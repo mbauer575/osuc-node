e26b39ee5b3166cc2bdb1f21922ea3b63a7feed1
mbauer575 <bauermax@oregonstate.edu>
2023-10-18 14:11:08 -0700

    fixed react router.

---
+++ client/src/Pages/secondFloor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+function secondFloor() {
+  return (
+    <div>
+        <Navbar />
+        <h1>2nd Floor</h1>
+    </div>
+  )
+}
+
+export default secondFloor
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import f1 from './Pages/f1';
+import FirstFloor from './Pages/firstFloor';
+import SecondFloor from './Pages/secondFloor';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter({
-  routes: [
-    { path: "/", component: App },
-    { path: "/f1", component: f1 },
-  ],
-});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/firstfloor",
+    element: <FirstFloor />,
+  },
+  {
+    path: "/secondfloor",
+    element: <SecondFloor />,
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
