d8043d62dfa28d73551bcb6654d709595a2129b3
mbauer575 <bauermax@oregonstate.edu>
2023-10-18 10:52:29 -0700

    Some react stuff (pre react router install)

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                Logo
+            </div>
+            <ul className="navbar-menu">
+                <li>Home</li>
+                <li>1st Floor</li>
+                <li>2nd Floor</li>
+                <li>3rd Floor</li>
+                <li>4th Floor</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar
--- server/index.js
+++ server/index.js
@@ -11,32 +11,13 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  // All other GET requests not handled before will return our React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+  res.json({ message: "Hello from server!" });
+});
 
-// app.use(express.static('public'))
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/img', express.static(__dirname + 'public/img'))
-
-// app.set('views', './views')
-// app.set('view engine', 'ejs')
-
-// app.get('', (req, res) => {
-//     res.render('index', { text: 'This is EJS' })
-// })
-// app.get('/index', (req, res) => {
-//     res.render('index', { text: 'This is EJS' })
-// })
-// app.get('/test1', (req, res) => {
-//     res.render('test1', { text: 'This is EJS' })
-// })
-
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
