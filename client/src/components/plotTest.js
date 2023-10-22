8858b307ca273575947dabcf8d351147ecb7b3a9
mbauer575 <bauermax@oregonstate.edu>
2023-10-21 22:02:53 -0700

    no werk

--- client/src/components/plotTest.js
+++ client/src/components/plotTest.js
@@ -2,19 +2,29 @@ import React from 'react'
 import Plot from 'react-plotly.js'
 
 function plotTest() {
+
+  const [data ,setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/testData')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  
   return (
     <Plot
         data={[
           {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
+            x: data.map((d)=> d.Time),
+            y: data.map((d) => d.Server1_meter1_avg),
             type: 'scatter',
             mode: 'lines+markers',
             marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          },  
         ]}
-        layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+        layout={{width: 320, height: 240, title: 'Test data'}}
       />
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const path = require('path');
 const express = require('express')
 const app = express()
 const PORT = 8080
+const sql = require('mssql')
 
+const config = {
+  user: 'your-username',
+  password: 'your-password',
+  server: 'sql-engr-cem-db.database.windows.net',
+  database: 'your-database-name',
+};
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -10,6 +17,17 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get('/api/testData', async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+    const result = await pool.request().query('SELECT * FROM TestData');
+    res.send(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 // All other GET requests not handled before will return our React app
